9bbf2cf5e0ed5e502e71ce2dee05c952ad961514
Ксения Финаева <polya.makova@outlook.com>
2022-01-18 22:40:27 +0300

    Добавьте файлы проекта.

---
+++ Task19_Wpf/Models/Arithmetics.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task19_Wpf.Models
+{
+    static class Arithmetics
+    {
+        public static double AddСircumference(double rad)
+        {
+            try
+            {
+                if (rad >= 0)
+                    return Math.Round((2 * Math.PI * rad), 4);
+                else
+                    return 0;
+            }
+            catch (FormatException ex)
+            {
+                rad.ToString(ex.Message);
+                return rad;
+            }
+        }
+        public static double AddSquare(double rad)
+        {
+            try
+            {
+                if (rad >= 0)
+                    return Math.Round((Math.PI * rad * rad), 4);
+                else
+                    return 0;
+            }
+            catch (FormatException ex)
+            {
+                rad.ToString(ex.Message);
+                return rad;
+            }
+        }
+    }
+}
---
+++ Task19_Wpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Task19_Wpf.Models;
+
+namespace Task19_Wpf.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double circumference;
+        public double Сircumference
+        {
+            get => circumference;
+            set
+            {
+                circumference = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double square;
+        public double Square
+        {
+            get => square;
+            set
+            {
+                square = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Сircumference = Arithmetics.AddСircumference(Radius);
+            Square = Arithmetics.AddSquare(Radius);
+        }
+
+        private bool CanAddCommandExecute(object p)
+        {
+            return true;
+        }
+
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecute);
+        }
+    }
+}
